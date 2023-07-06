4a0d4ca15ab85f66bbb6112f7aa809f6fb47f8a7
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2023-07-06 15:40:39 +0800

    refactored dsl

--- dsl.py
+++ dsl.py
@@ -1,134 +1,124 @@
+from __future__ import annotations
 from chiquito_ast import Circuit, StepType
 from typing import Callable
-from query import Queriable
+from query import Queriable, Forward, Internal, Shared, Fixed, StepTypeNext
 from expr import Expr
 from util import uuid
-from cb import Constraint
+from cb import Constraint, ToConstraint, to_constraint, Typing
+from dataclasses import dataclass
 
 class CircuitContext:
     def __init__(self):
         self.circuit = Circuit()
 
-    def forward(self, name: str) -> Queriable:
-        out = Queriable("Forward")
-        forward_signal = self.circuit.add_forward(name, 0)
-        out.data['forward'] = forward_signal
-        out.data['rotation'] = 0
-        return out
+    def forward(self: CircuitContext, name: str) -> Queriable:
+        return Queriable(Forward(self.circuit.add_forward(name, 0), False))
 
-    def forward_with_phase(self, name: str, phase: int) -> Queriable:
-        out = Queriable("Forward")
-        forward_signal = self.circuit.add_forward(name, phase)
-        out.data['forward'] = forward_signal
-        out.data['rotation'] = 0
-        return out
+    def forward_with_phase(self: CircuitContext, name: str, phase: int) -> Queriable:
+        return Queriable(Forward(self.circuit.add_forward(name, phase), False))
     
-    def shared(self, name: str) -> Queriable:
-        out = Queriable("Shared")
-        shared_signal = self.circuit.add_shared(name, 0)
-        out.data['shared'] = shared_signal
-        out.data['rotation'] = 0
-        return out
+    def shared(self: CircuitContext, name: str) -> Queriable:
+        return Queriable(Shared(self.circuit.add_shared(name, 0), 0))
     
-    def shared_with_phase(self, name: str, phase: int) -> Queriable:
-        out = Queriable("Shared")
-        shared_signal = self.circuit.add_shared(name, phase)
-        out.data['shared'] = shared_signal
-        out.data['rotation'] = 0
-        return out
-    
-    def fixed(self, name: str) -> Queriable:
-        out = Queriable("Fixed")
-        fixed_signal = self.circuit.add_fixed(name)
-        out.data['fixed'] = fixed_signal
-        out.data['rotation'] = 0
-        return out
-    
-    def expose(self, forward: Queriable) -> None:
-        if forward.variant != "Forward":
-            raise ValueError("Can only expose a forward signal.")
-        else: 
-            self.circuit.expose(forward.data['forward'])
+    def shared_with_phase(self: CircuitContext, name: str, phase: int) -> Queriable:
+        return Queriable(Shared(self.circuit.add_shared(name, phase), 0))
 
-    # TODO: Implement import_halo2_advice and import_halo2_fixed. Currently we ignore imported query types
+    def fixed(self: CircuitContext, name: str) -> Queriable:
+        return Queriable(Fixed(self.circuit.add_fixed(name), 0))
+    
+    def expose(self: CircuitContext, forward: Queriable):
+        match forward:
+            case Queriable(Forward(forward_signal, _)):
+                self.circuit.expose(forward_signal)
+            case _:
+                raise ValueError("Can only expose a forward signal.")
+            
+    # TODO: Implement import_halo2_advice and import_halo2_fixed. Currently we ignore imported query types.
 
-    def step_type(self, name: str) -> 'StepTypeHandler':
+    def step_type(self: CircuitContext, name: str) -> StepTypeHandler:
         handler = StepTypeHandler.new(name)
         self.circuit.add_step_type(handler, name)
         return handler
     
-    def step_type_def(self, step: 'StepTypeDefInput', func: Callable[['StepTypeContext'], 'StepTypeWGHandler']) -> 'StepTypeWGHandler':
-        if step.variant == "Handler":
-            handler = step.data['handler']
-        elif step.variant == "String":
-            handler = StepTypeHandler.new(step.data['string'])
-            self.circuit.add_step_type(handler, step.data['string'])
-        else:
-            raise ValueError("Invalid StepTypeDefInput variant.")
-        context = StepTypeContext.new(handler.uuid(), handler.annotation)
-        result = func(context)
+    def step_type_def(self: CircuitContext, step: StepTypeDefInput, step_type_func: Callable[[StepTypeContext], StepTypeWGHandler]) -> StepTypeWGHandler:
+        match step:
+            case StepTypeDefInput(Handler(h)):
+                handler = h
+            case StepTypeDefInput(String(name)):
+                handler = StepTypeHandler.new(name)
+                self.circuit.add_step_type(handler, name)
+            case _:
+                raise ValueError("Invalid StepTypeDefInput enum type.")
+        context = StepTypeContext.new(handler.id, handler.annotation)
+        result = step_type_func(context)
         self.circuit.add_step_type_def(context.step_type)
         return result
     
-    def trace(self, func: Callable[['TraceContext', 'TraceArgs'], None]) -> None:
-        self.circuit.set_trace(func)
+    def trace(self: CircuitContext, trace_def: Callable[[TraceContext, TraceArgs], None]):
+        self.circuit.set_trace(trace_def)
 
-    def pragma_first_step(self, step_type: 'StepTypeHandler') -> None:
-        self.circuit.first_step = step_type.uuid()
+    def fixed_gen(self: CircuitContext, fixed_gen_def: Callable[[FixedGenContext], None]):
+        self.circuit.set_fixed_gen(fixed_gen_def)
 
-    def pragma_last_step(self, step_type: 'StepTypeHandler') -> None:
-        self.circuit.last_step = step_type.uuid()
+    def pragma_first_step(self: CircuitContext, step_type: StepTypeHandler) -> None: # TODO: define and use ToStepTypeHandler instead
+        self.circuit.first_step = step_type.id
+
+    def pragma_last_step(self, step_type: StepTypeHandler) -> None:
+        self.circuit.last_step = step_type.id
     
     def pragma_num_steps(self, num_steps: int) -> None:
         self.circuit.num_steps = num_steps
 
+@dataclass
+class Handler:
+    handler: StepTypeHandler
+
+@dataclass
+class String:
+    string: str
+
+@dataclass
 class StepTypeDefInput:
-    def __init__(self, variant: str):
-        if variant not in ['Handler', 'String']:
-            raise ValueError("Invalid StepTypeDefInput variant.")
-        self.variant = variant
-        self.data = {}
-        self.data['step_type_handler']: StepTypeHandler = None
-        self.data['string']: str = None
+    enum: Handler | String
     
-    def from_handler(h: 'StepTypeHandler') -> 'StepTypeDefInput':
-        out = StepTypeDefInput('Handler')
-        out.data['step_type_handler'] = h
-        return out
+    def from_handler(h: StepTypeHandler) -> StepTypeDefInput:
+        return StepTypeDefInput(Handler(h))
     
-    def from_string(s: str) -> 'StepTypeDefInput':
-        out = StepTypeDefInput('String')
-        out.data['string'] = s
-        return out
+    def from_string(s: str) -> StepTypeDefInput:
+        return StepTypeDefInput(String(s))
 
+@dataclass
 class StepTypeContext:
-    def __init__(self, step_type: StepType):
-        self.step_type = step_type
+    step_type: StepType
     
-    def new(id: int, name: str) -> 'StepTypeContext':
+    def new(id: int, name: str) -> StepTypeContext:
         return StepTypeContext(StepType(id, name))
-
-    def setup(self, func: Callable[['StepTypeSetupContext'], None]) -> None: # def is a keyword in python
-        ctx = StepTypeSetupContext(self.step_type)
-        func(ctx)
     
-    def wg(self, func: Callable[['StepInstance', 'Args'], None]) -> 'StepTypeWGHandler':
-        handler = StepTypeWGHandler(
-            self.step_type.uuid(), 
+    def internal(self: StepTypeContext, name: str) -> Queriable:
+        return Queriable(Internal(self.step_type.add_signal(name)))
+
+    def setup(self, setup_def: Callable[[StepTypeSetupContext], None]) -> None: # def is a keyword in python
+        ctx = StepTypeSetupContext(self.step_type)
+        setup_def(ctx)
+    
+    def wg(self, wg_def: Callable[[StepInstance, Args], None]) -> StepTypeWGHandler:
+        return StepTypeWGHandler(
+            self.step_type.id, 
             self.step_type.name,
-            func
-            )
-        return handler
+            wg_def
+        )
 
+@dataclass
 class StepTypeSetupContext:
-    def __init__(self, step_type: StepType):
-        self.step_type = step_type
+    step_type: StepType
 
-    def constr(self, constraint: Constraint) -> None:
+    def constr(self: StepTypeSetupContext, constraint: ToConstraint):
+        constraint = to_constraint(constraint)
         StepTypeSetupContext.enforce_constraint_typing(constraint)
         self.step_type.add_constr(constraint.annotation, constraint.expr)
 
-    def transition(self, constraint: Constraint):
+    def transition(self: StepTypeSetupContext, constraint: ToConstraint):
+        constraint = to_constraint(constraint)
         StepTypeSetupContext.enforce_constraint_typing(constraint)
         self.step_type.add_transition(constraint.annotation, constraint.expr)
 
@@ -136,45 +126,29 @@ class StepTypeSetupContext:
         if constraint.typing != Typing.AntiBooly:
             raise ValueError(f"Expected AntiBooly constraint, got {constraint.typing} (constraint: {constraint.annotation})")
 
-    # TODO: implement add_lookup after lookup abstraction PR is merged.
+    # TODO: Implement add_lookup after lookup abstraction PR is merged.
 
+@dataclass
 class StepTypeHandler:
-    def __init__(self, id: int, annotation: str):
-        self.id = id
-        self.annotation = annotation
+    id: int
+    annotation: str
 
-    def new(annotation: str) -> 'StepTypeHandler':
+    def new(annotation: str) -> StepTypeHandler:
         return StepTypeHandler(uuid(), annotation)
-
-    def uuid(self) -> int:
-        return self.id
     
-    def next(self) -> Queriable:
-        out = Queriable("StepTypeNext")
-        out.data['step_type_next'] = self
-        return out
+    def next(self: StepTypeHandler) -> Queriable:
+        return Queriable(StepTypeNext(self))
     
-# impl<F, Args, D: Fn(&mut StepInstance<F>, Args) + 'static> From<&StepTypeWGHandler<F, Args, D>>
-#     for StepTypeHandler
-# {
-#     fn from(h: &StepTypeWGHandler<F, Args, D>) -> Self {
-#         StepTypeHandler {
-#             id: h.id,
-#             annotation: h.annotation,
-#         }
-#     }
-# }
+    def from_wg_handler(wg_handler: StepTypeWGHandler) -> StepTypeHandler:
+        return StepTypeHandler(wg_handler.id, wg_handler.annotation)
 
+@dataclass
 class StepTypeWGHandler:
-    def __init__(self, id: int, annotation: str, wg: Callable[['StepInstance', 'Args'], None]):
-        self.id = id
-        self.annotation = annotation
-        self.wg = wg
-    
-    def uuid(self) -> int:
-        return self.id
+    id: int
+    annotation: str
+    wg: Callable[[StepInstance, Args], None]
 
-def circuit(name: str, circuit_context_lambda: Callable[[CircuitContext], None]) -> Circuit:
+def circuit(name: str, circuit_context_def: Callable[[CircuitContext], None]) -> Circuit:
     ctx = CircuitContext()
-    circuit_context_lambda(ctx)
+    circuit_context_def(ctx)
     return ctx.circuit
