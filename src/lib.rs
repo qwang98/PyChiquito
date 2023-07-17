05579c99a6e3aebf0cabad3088355f00baddbab0
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2023-07-17 17:17:20 +0800

    added scripts for .env

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use chiquito::ast::Circuit as cCircuit;
 
 
 #[pyfunction]
-fn print_ast(json: &PyString) {
+fn convert_and_print_ast(json: &PyString) {
     let deserialized_ast: Circuit<u32> = serde_json::from_str(json.to_str().expect("PyString convertion failed.")).expect("Json deserialization to Circuit failed.");
     let chiquito_ast: cCircuit<u32, ()> = to_chiquito_ast::<()>(deserialized_ast);
     println!("{:?}", chiquito_ast);
@@ -18,6 +18,6 @@ fn print_ast(json: &PyString) {
 
 #[pymodule]
 fn rust_chiquito(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(print_ast, m)?)?;
+    m.add_function(wrap_pyfunction!(convert_and_print_ast, m)?)?;
     Ok(())
 }
