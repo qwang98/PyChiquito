1653b59c1b91b0c3e7e27737be7fb9f667cfaf6a
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2023-07-20 19:45:57 +0800

    generate proof from halo2 using python; pragma first and last step are bugging out in fibo

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use chiquito::{ast::Circuit, wit_gen::TraceWitness};
+use chiquito::{ast::Circuit, wit_gen::TraceWitness, frontend::pychiquito::{chiquito_ast_to_halo2, chiquito_verify_proof}};
 use halo2_proofs::halo2curves::bn256::Fr;
-use pyo3::{prelude::*, types::PyString};
+use pyo3::{prelude::*, types::{PyString, PyLong}};
 
 #[pyfunction]
 fn convert_and_print_ast(json: &PyString) {
@@ -18,9 +18,24 @@ fn convert_and_print_trace_witness(json: &PyString) {
     println!("{:?}", trace_witness);
 }
 
+#[pyfunction]
+fn ast_to_halo2(json: &PyString) -> u128 {
+    let uuid = chiquito_ast_to_halo2(json.to_str().expect("PyString convertion failed."));
+    println!("{:?}", uuid);
+
+    uuid
+}
+
+#[pyfunction]
+fn verify_proof(witness_json: &PyString, ast_uuid: &PyLong) {
+    chiquito_verify_proof(witness_json.to_str().expect("PyString convertion failed."), ast_uuid.extract().expect("PyLong convertion failed."));
+}
+
 #[pymodule]
 fn rust_chiquito(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(convert_and_print_ast, m)?)?;
     m.add_function(wrap_pyfunction!(convert_and_print_trace_witness, m)?)?;
+    m.add_function(wrap_pyfunction!(ast_to_halo2, m)?)?;
+    m.add_function(wrap_pyfunction!(verify_proof, m)?)?;
     Ok(())
 }
