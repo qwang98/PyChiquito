35726a82cd0c49a34ee18821d011da793afefd00
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2023-08-14 18:57:47 +0800

    updated tutorial

--- pychiquito/fibonacci.py
+++ pychiquito/fibonacci.py
@@ -5,63 +5,102 @@ from dsl import Circuit, StepType
 from cb import eq
 from query import Queriable
 from util import F
+import rust_chiquito
 
 
-class Fibonacci(Circuit):
-    def setup(self: Fibonacci):
-        self.a: Queriable = self.forward("a")
-        self.b: Queriable = self.forward("b")
+# class Fibonacci(Circuit):
+#     def setup(self: Fibonacci):
+#         self.a: Queriable = self.forward("a")
+#         self.b: Queriable = self.forward("b")
 
-        self.fibo_step = self.step_type(FiboStep(self, "fibo_step"))
-        self.fibo_last_step = self.step_type(FiboLastStep(self, "fibo_last_step"))
+#         self.fibo_step = self.step_type(FiboStep(self, "fibo_step"))
+#         self.fibo_last_step = self.step_type(FiboLastStep(self, "fibo_last_step"))
 
-        self.pragma_first_step(self.fibo_step)
-        self.pragma_last_step(self.fibo_last_step)
-        self.pragma_num_steps(11)
-        # self.pragma_disable_q_enable()
+#         self.pragma_first_step(self.fibo_step)
+#         self.pragma_last_step(self.fibo_last_step)
+#         self.pragma_num_steps(11)
+#         # self.pragma_disable_q_enable()
 
-        # self.expose(self.b, First())
-        # self.expose(self.a, Last())
-        # self.expose(self.a, Step(1))
+#         # self.expose(self.b, First())
+#         # self.expose(self.a, Last())
+#         # self.expose(self.a, Step(1))
 
-    def trace(self: Fibonacci, args: Any):
-        self.add(self.fibo_step, (1, 1))
-        a = 1
-        b = 2
-        for i in range(1, 10):
-            self.add(self.fibo_step, (a, b))
-            prev_a = a
-            a = b
-            b += prev_a
-        self.add(self.fibo_last_step, (a, b))
+#     def trace(self: Fibonacci, args: Any):
+#         self.add(self.fibo_step, (1, 1))
+#         a = 1
+#         b = 2
+#         for i in range(1, 10):
+#             self.add(self.fibo_step, (a, b))
+#             prev_a = a
+#             a = b
+#             b += prev_a
+#         self.add(self.fibo_last_step, (a, b))
 
+
+# class FiboStep(StepType):
+#     def setup(self: FiboStep):
+#         self.c = self.internal("c")
+#         self.constr(eq(self.circuit.a + self.circuit.b, self.c))
+#         self.transition(eq(self.circuit.b, self.circuit.a.next()))
+#         self.transition(eq(self.c, self.circuit.b.next()))
+
+#     def wg(self: FiboStep, args: Tuple[int, int]):
+#         a_value, b_value = args
+#         self.assign(self.circuit.a, F(a_value))
+#         self.assign(self.circuit.b, F(b_value))
+#         self.assign(self.c, F(a_value + b_value))
+
+
+# class FiboLastStep(StepType):
+#     def setup(self: FiboLastStep):
+#         self.c = self.internal("c")
+#         self.constr(eq(self.circuit.a + self.circuit.b, self.c))
+
+#     def wg(self: FiboLastStep, values=Tuple[int, int]):
+#         a_value, b_value = values
+#         self.assign(self.circuit.a, F(a_value))
+#         self.assign(self.circuit.b, F(b_value))
+#         self.assign(self.c, F(a_value + b_value))
+
+
+# fibo = Fibonacci()
+# fibo_witness = fibo.gen_witness(None)
+# fibo.halo2_mock_prover(fibo_witness)
 
 class FiboStep(StepType):
-    def setup(self: FiboStep):
+    def setup(self):
         self.c = self.internal("c")
         self.constr(eq(self.circuit.a + self.circuit.b, self.c))
         self.transition(eq(self.circuit.b, self.circuit.a.next()))
         self.transition(eq(self.c, self.circuit.b.next()))
 
-    def wg(self: FiboStep, args: Tuple[int, int]):
+    def wg(self, args):
         a_value, b_value = args
         self.assign(self.circuit.a, F(a_value))
         self.assign(self.circuit.b, F(b_value))
         self.assign(self.c, F(a_value + b_value))
 
-
-class FiboLastStep(StepType):
-    def setup(self: FiboLastStep):
-        self.c = self.internal("c")
-        self.constr(eq(self.circuit.a + self.circuit.b, self.c))
-
-    def wg(self: FiboLastStep, values=Tuple[int, int]):
-        a_value, b_value = values
-        self.assign(self.circuit.a, F(a_value))
-        self.assign(self.circuit.b, F(b_value))
-        self.assign(self.c, F(a_value + b_value))
-
+class Fibonacci(Circuit):
+    def setup(self):
+        self.a = self.forward("a")
+        self.b = self.forward("b")
+        
+        self.fibo_step = self.step_type(FiboStep(self, "fibo_step"))
+        self.pragma_num_steps(4)
+        self.pragma_first_step(self.fibo_step)
+        
+    def trace(self, args):
+        self.add(self.fibo_step, (1, 1))
+        a = 1
+        b = 2
+        for i in range(1, 4):
+            self.add(self.fibo_step, (a, b))
+            prev_a = a
+            a = b
+            b += prev_a
 
 fibo = Fibonacci()
+print(fibo)
+rust_chiquito.convert_and_print_ast(fibo.get_ast_json())
 fibo_witness = fibo.gen_witness(None)
 fibo.halo2_mock_prover(fibo_witness)
